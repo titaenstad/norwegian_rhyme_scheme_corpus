4d8b4ab468c0a9cc907d631bfefae1547811d565
Tita <tita.enstad@gmail.com>
2022-02-22 15:24:58 +0100

    Fix bug in cleanup code in after annotation script

--- annotation_tool/annotations_to_tsv.py
+++ annotation_tool/annotations_to_tsv.py
@@ -1,7 +1,36 @@
 import argparse
+import string 
 from pathlib import Path
 import pandas as pd
 
+
+def clean_scheme(scheme):
+    letters = "---"+string.ascii_uppercase.replace("T", "").replace("I","").replace("N","")
+
+    def scheme_to_numeric(scheme):
+        scheme = scheme.replace("T", "0")
+        scheme = scheme.replace("I", "1")
+        scheme = scheme.replace("N", "2")
+        unique_chars = (set(scheme) - {"0", "1", "2"})
+        unique_chars = sorted(list(unique_chars), key=lambda x: scheme.index(x))
+        scheme = "-".join(scheme)    
+        for i, c in enumerate(unique_chars, start=3):
+            scheme = scheme.replace(c, str(i))
+        return scheme
+
+    def numeric_to_scheme(scheme):
+        unique_chars = sorted(list(set(scheme.split("-")) - {"0", "1", "2"}), key=int)
+        for i, c in enumerate(unique_chars, start=3):
+            scheme = scheme.replace(c, letters[i])
+        scheme = scheme.replace("-", "")
+        scheme = scheme.replace("0", "T")
+        scheme = scheme.replace("1", "I")
+        scheme = scheme.replace("2", "N")
+        return scheme
+
+    scheme = scheme_to_numeric(scheme)
+    scheme = numeric_to_scheme(scheme)
+    return scheme
 
 def annotations_to_tsv(args):
     """Retrieve annotations and save as tsv
@@ -10,7 +39,7 @@ def annotations_to_tsv(args):
         args (argparse.Namespace): dict containing path to directory to read from
     """
     dst =  Path(args.dst)
-    codes = []
+    schemes = []
     stanzas = []
 
     for e in dst.iterdir():
@@ -18,10 +47,12 @@ def annotations_to_tsv(args):
             st = e.read_text().split("\n\n")[:-1]
             for s in st:
                 lines = s.split("\n")
-                codes.append(lines[0])
+                scheme = clean_scheme(lines[0])
+                schemes.append(scheme)
                 stanzas.append("\n".join(lines[1:]))
 
-    poem_df = pd.DataFrame({"rhyme scheme":codes, "stanza": stanzas})
+    poem_df = pd.DataFrame({"rhyme scheme":schemes, "stanza": stanzas})
+
     poem_df.to_csv(f"tsvs/{dst.name}_rhymes_poems.tsv", sep="\t", index=False)
 
     print(f"All annotations saved to <tsvs/{dst.name}_rhymes_poems.tsv>.")
